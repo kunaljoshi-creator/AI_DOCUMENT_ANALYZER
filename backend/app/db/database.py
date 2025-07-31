fff205b02cf91c3ac656205b4ec8d71f1df9f898
kunal joshi <127673576+kunaljoshi-creator@users.noreply.github.com>
2025-07-31 11:12:26 +0530

    Update database.py

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -4,7 +4,12 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
+# If not already in your DATABASE_URL, you can force sslmode=require like this:
+engine = create_engine(
+    settings.DATABASE_URL,
+    connect_args={"sslmode": "require"} if "render.com" in settings.DATABASE_URL else {}
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
